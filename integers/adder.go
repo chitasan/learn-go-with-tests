cbddde492d019f7e4cd287bba66aebd4025eed66
Chi Tran <chidtran@gmail.com>
2020-06-02 19:09:23 -0400

    Add Adder with Example

---
+++ integers/adder.go
@@ -0,0 +1,7 @@
+package integers
+
+func Add(x, y int) int {
+	return x + y
+}
+
+// when there are more than two args of the same type, they can be shortened as (arg1, arg2 type) - so (x, y int) rather than (x int, y int)
